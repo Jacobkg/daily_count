fdd0f889ad91bb5c445bb0fa090458ba9637b3ed
Jacob Green <jacob@nationbuilder.com>
2013-02-14 08:35:01 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyCount::Application.config.secret_token = 'de24574c2fc34a2122d9d15c1db497580b5c4044d0ac6124b2c7667e5926ee5f63464f76c2867d929f745d303f9b32a53ed04a0506519b012cff97e919ca0120'
