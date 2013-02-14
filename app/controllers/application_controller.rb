daf12418fa00d7ab8d5c6e09c15b6047c121c6bd
Jacob Green <jacob@nationbuilder.com>
2013-02-14 09:37:54 -0800

    Add Mixpanel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   if Rails.env.production?
     http_basic_authenticate_with :name => ENV['USERNAME'], :password => ENV['PASSWORD']
   end
+
+  def mixpanel
+    @mixpanel ||= Mixpanel::Tracker.new "09c7d977aaf4cff6491ea764ac87a0b3", { :env => request.env }
+  end
 end
--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -7,6 +7,11 @@ class CountsController < ApplicationController
     count = Count.new
     count.record_date = Date.today
     count.save!
+
+    if Rails.env.production?
+      mixpanel.track 'Activity Recorded', {}
+    end
+
     redirect_to :action => 'index'
   end
 
